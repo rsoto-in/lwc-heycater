d98ceabc2169c0da38e8f816f900eb9fbcc5cb03
Richard Soto <richardsoto@outlook.com>
2021-06-27 03:02:15 +0200

    Add improvements for Invoice Line Items LWC

--- force-app/main/default/lwc/invoiceLineItems/invoiceLineItems.js
+++ force-app/main/default/lwc/invoiceLineItems/invoiceLineItems.js
@@ -1,11 +1,14 @@
 import { LightningElement, api, track, wire } from 'lwc';
 import { getObjectInfos } from 'lightning/uiObjectInfoApi';
+import { getRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { CloseActionScreenEvent } from 'lightning/actions';
+
 import getInvoiceLineItems from '@salesforce/apex/InvoiceLineItemsController.getInvoiceLineItems';
 
 import CURRENCY from '@salesforce/i18n/currency';
-
 import INVOICE_OBJECT from '@salesforce/schema/Invoice__c';
+import INVOICE_ID_FIELD from '@salesforce/schema/Invoice__c.Id';
 import INVOICE_NAME_FIELD from '@salesforce/schema/Invoice__c.Name';
 import INVOICE_AMOUNT_ILI_FIELD from '@salesforce/schema/Invoice__c.Amount_Invoice_Line_Items__c';
 import INVOICE_LINE_ITEM_OBJECT from '@salesforce/schema/Invoice_Line_Item__c';
@@ -17,67 +20,96 @@ const COLS = [
 	{label: 'Amount', fieldName: 'Amount__c', type: 'currency', typeAttributes: { currencyCode: CURRENCY, step: '0.001'}}
 ];
 
-export default class InvoiceLineItems extends LightningElement {
+const ERROR_TITLE = 'We encountered a problem when trying to display Invoice Line Items.';
+const ERROR_MSG = 'For help, contact your administrator';
+const ERROR_VAR = 'error';
 
+export default class InvoiceLineItems extends LightningElement
+{
 	@api
 	recordId;
 
-	objectApiNames = [INVOICE_OBJECT, INVOICE_LINE_ITEM_OBJECT];
+	@track
+	error;
+	
+	errorVariant = ERROR_VAR;
+	errorTitle = ERROR_TITLE;
+	errorMessage = ERROR_MSG;
 	
 	invoiceLabel = '';
 	itemLabel = '';
-
+	objectApiNames = [INVOICE_OBJECT, INVOICE_LINE_ITEM_OBJECT];
 	invoice = INVOICE_OBJECT;
 	invoiceName = INVOICE_NAME_FIELD;
 	invoiceAmountILIs = INVOICE_AMOUNT_ILI_FIELD;
-
+	close = CLOSE;
 	columns = COLS;
 	
 	@track 
-	itemsList = [];
-
-	length = -1;
-
-	close = CLOSE;
+	items = [];
 
 	@wire(getObjectInfos, { objectApiNames: '$objectApiNames' })
-	objectInfos(result) {
+	objectInfos(result)
+	{
 		const {data, error} = result;
-		if (data) {
+		if (data)
+		{
 			this.invoiceLabel = data.results[0].result.label;
 			this.itemLabel = data.results[1].result.label;
 		}
-		if (error) {
+		if (error)
+		{
             this.error = error;
         }
 	};
 
-	get items() {
-		if (this.length < 0)
+	@wire(getRecord, { recordId: '$recordId', fields: [INVOICE_ID_FIELD] })
+    record(result)
+	{
+		const {data, error} = result;
+		if (data)
 		{
-			getInvoiceLineItems({ invoiceId: this.recordId })
-				.then((data) => {
-					let nameUrl;
-					this.itemsList = data.map(row => {
-						nameUrl = `/${row.Id}`;
-						return {...row , nameUrl} 
-					})
-					this.error = undefined;
-				})
-				.catch((error) => {
-					this.error = error;
-					this.itemsList = undefined;
-				});
-			if (this.error) {
-				this.length = 0;
-				return false;
-			}
-			this.length = this.itemsList.length;
+			this.retrieveItems();
 		}
-		return true;
+		if (error)
+		{
+			this.error = error;
+        }
+	};
+
+	retrieveItems()
+	{
+		getInvoiceLineItems({ invoiceId: this.recordId })
+			.then((data) => 
+			{
+				let nameUrl;
+				this.items = data.map(row => 
+				{
+					nameUrl = `/${row.Id}`;
+					return {...row , nameUrl} 
+				})
+				this.error = undefined;
+			})
+			.catch((error) => 
+			{
+				this.showError();
+				this.error = error;
+				this.items = undefined;
+			});
 	}
 
-	handleClose() {
+	showError()
+	{
+        const event = new ShowToastEvent({
+            title: this.errorTitle,
+            message: this.errorMessage,
+            variant: this.errorVariant,
+        });
+        this.dispatchEvent(event);
+    }
+
+	handleClose()
+	{
 		this.dispatchEvent(new CloseActionScreenEvent());
 	}
 
